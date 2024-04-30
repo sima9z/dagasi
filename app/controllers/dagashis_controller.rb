7788a22de51f96d158363d7b1f9a1f9d3a862a90
sima9z <159206331+sima9z@users.noreply.github.com>
2024-04-30 20:28:25 +0900

    お品書き作った

--- app/controllers/dagashis_controller.rb
+++ app/controllers/dagashis_controller.rb
@@ -1,10 +1,13 @@
 class DagashisController < ApplicationController
+  
   def index
+    @dagashis=Dagashi.all
   end
+
   def purchase
     budget = params[:budget].to_i
     dagashis = Dagashi.where('price <= ?', budget)
-    selected_dagashis = []
+    dagashi_counts = Hash.new(0) # 駄菓子の選択回数を記録するハッシュ
     remaining_budget = budget
   
     30.times do
@@ -12,10 +15,14 @@ class DagashisController < ApplicationController
       break if affordable_dagashis.empty?
   
       dagashi = affordable_dagashis.sample
-      selected_dagashis << dagashi
+      dagashi_counts[dagashi] += 1 # 選んだ駄菓子のカウントを増やす
       remaining_budget -= dagashi.price
     end
   
-    @dagashis = selected_dagashis
+    # カウントを基に表示用の配列を作成
+    @dagashis = dagashi_counts.map do |dagashi, count|
+      { name: dagashi.name, price: dagashi.price, count: count }
+    end
   end
+  
 end
