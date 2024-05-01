8f045cbd5ad4a7402e61dd6b25171736122dc7c0
sima9z <159206331+sima9z@users.noreply.github.com>
2024-05-01 18:57:36 +0900

    デプロイ修正15

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,6 @@ module Dagasi
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
-    if Rails.env.production?
-      Dotenv::Rails.load
-    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
