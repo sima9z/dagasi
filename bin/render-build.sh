9a0704fc2058448bfb7757ae513c24699174d73c
sima9z <159206331+sima9z@users.noreply.github.com>
2024-05-02 18:34:49 +0900

    デプロイ修正21

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rails webpacker:compile
 bundle exec rake db:migrate
 bundle exec rails db:seed
